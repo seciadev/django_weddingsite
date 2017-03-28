0c2b6148c0278c6c1d3e5f365adf79f0fbc320db
Gianluca Fandella <Gianluca Fandella>
2017-03-28 20:32:17 +0200

    Update wsgi.py settings

--- weddingsite/wsgi.py
+++ weddingsite/wsgi.py
@@ -12,7 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weddingsite.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weddingsite.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'weddingsite.settings'
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
